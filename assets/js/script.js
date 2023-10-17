941503eaf06d03d23bf6d9a2a503e4be77de95c7
srgowen <srgowen2@gmail.com>
2023-10-16 21:50:15 -0400

    working recipe api, good code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,50 @@
-const axios = require('axios');
+const searchButton = document.querySelector("#searchButton");
+const recipeResults = document.querySelector("#recipeResults");
+const textBox = document.querySelector("#textBox");
+const apiKey = "2563b3155a0747ec8b0c5c273a182aff";
+let query = "pasta";
+let cuisine = "Italian";
 
-const apiKey = '2563b3155a0747ec8b0c5c273a182aff';
-const query = 'pasta';
-const cuisine = 'Italian';
+function searchRecipes(event) {
+  event.preventDefault();
+  query = textBox.value;
+  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}&cuisine=${cuisine}`;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then((responseData) => {
+      const recipes = responseData.results;
 
-const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}&cuisine=${cuisine}`;
-
-axios.get(url)
-  .then((response) => {
-    const recipes = response.data.results;
-
-    recipes.forEach((recipe) => {
-      console.log(recipe.title);
+      recipes.forEach((recipe) => {
+        console.log(recipe);
+        const recipeDiv = document.createElement("div");
+        recipeDiv.textContent = recipe.title;
+        recipeDiv.addEventListener("click", function(event) {
+            event.preventDefault();
+            searchActualRecipe(recipe.id)
+        });
+        recipeResults.appendChild(recipeDiv);
+      });
+    })
+    .catch((error) => {
+      console.error("An error occurred:", error);
     });
-  })
-  .catch((error) => {
-    console.error('An error occurred:', error);
-  });
+}
 
+function searchActualRecipe(recipeID) {
+  const url = `https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=${apiKey}&includeNutrition=false`;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then((responseData) => {
+      const recipe = responseData;
+        console.log(recipe)
+    })
+    .catch((error) => {
+      console.error("An error occurred:", error);
+    });
+}
 
+searchButton.addEventListener("click", searchRecipes);
